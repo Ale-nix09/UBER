68157af77c9e3942c26f690b3cfe5d405d1fa009
DaniloMCD <danilo.granados2026@hilasal.superate.org.sv>
2025-07-13 22:34:36 -0600

    Pagina principal terminada

--- components/mainPage.js
+++ components/mainPage.js
@@ -2,18 +2,20 @@ class mainPage extends HTMLElement{
     connectedCallback() {
         this.innerHTML = `
         <div id="fondo">
-        <div class="flex pt-6">
+        <div class="flex justify-between  pt-6">
 
-        <section class="pl-7 pr-6">
+        <section class="pl-7 pr-80">
             <img src="images/Uber_logo_2018.png" alt="" width="110px">
         </section>
 
-        <section class="pl-20 pb-1">
+        <section class="flex pl-80 pr-5">
+        <section class="pb-1">
             <button class="hover:bg-gray-300 bg-white pt-2 pb-2 pl-2 pr-3 rounded-full font-bold text-lg flex text-stone-800"><img class="w-8 rounded-full pr-1" src="images/español.jpg" alt="">Es</button>
         </section>
             
         <section class="pl-4 pb-1 flex justify-center">
             <button class="hover:bg-gray-300 bg-white pt-2 pb-2 pl-3 pr-4 rounded-full font-bold text-lg flex text-stone-800"><img class="h-6 pr-2 mt-1" src="images/amburgesa.png" alt="">Menú</button>
+        </section>
         </section>
     </div>
 
@@ -24,7 +26,7 @@ class mainPage extends HTMLElement{
         <h1>de acuerdo</h1>
     </div>
 
-    <div class="pl-4 pt-45">
+    <div class="pl-4 pt-2">
         <button class="hover:bg-gray-800 py-4 px-12 font-semibold text-xl bg-zinc-900 text-white rounded-xl ">Descarga la app</button>
     </div>
     </div>
